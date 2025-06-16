a7f62c8a71440c487d865bcc90d84ad72d1e2a62
karan kumar khadria <karankhadria65@gmail.com>
2025-06-16 23:02:01 +0530

    add user and video model

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -5,7 +5,8 @@ class ApiResponse {
         message ="Success"
     ){
         this.statusCode = statusCode
-        this.data = datathis.message = message
+        this.data = data
+        this.message = message
         this.success = statusCode < 400
     }
 }
