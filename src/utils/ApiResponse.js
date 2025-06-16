59c68725ec4a5ea64395074b342025370a299e32
karan kumar khadria <karankhadria65@gmail.com>
2025-06-16 17:23:35 +0530

    util code discussion

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,11 @@
+class ApiResponse {
+    constructor(
+        statusCode,
+        data,
+        message ="Success"
+    ){
+        this.statusCode = statusCode
+        this.data = datathis.message = message
+        this.success = statusCode < 400
+    }
+}
